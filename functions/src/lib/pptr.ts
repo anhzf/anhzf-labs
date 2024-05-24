174fe7e35d2640ce04ee850fefb18bf78b158f00
Anhzf <me@anhzf.dev>
2024-05-24 22:46:25 +0700

    feat(fns): Add timeout option for waitEvent in getPdf function

--- functions/src/lib/pptr.ts
+++ functions/src/lib/pptr.ts
@@ -2,6 +2,8 @@
 import type {Browser, PDFOptions} from "puppeteer";
 import {launch} from "puppeteer";
 
+const DEFAULT_WAIT_EVENT_TIMEOUT = 45_000;
+
 let $: Browser | undefined;
 
 const getBrowser = async () => {
@@ -20,17 +22,41 @@ const closeBrowser = async () => {
   }
 };
 
+/**
+ * Options for generating a PDF using Puppeteer.
+ */
 interface GetPdfOptions extends PDFOptions {
+  /**
+   * Wait until the page fires the `window.readyToPrint()`
+   * instead of waiting for the network to become idle.
+   */
   waitEvent?: boolean;
+
+  /**
+   * The timeout (in milliseconds) for waiting for the `waitEvent` to be fired.
+   * If the timeout is reached, the page will be forced to print.
+   */
+  waitEventTimeout?: number;
 }
 
 export async function getPdf(url: string, opts?: GetPdfOptions) {
   const browser = await getBrowser();
 
   const page = await browser.newPage();
-  const pending = new Promise<void>(
-    (resolve) => page.exposeFunction("readyToPrint", resolve)
-  );
+  const pending = Promise.race([
+    new Promise<void>(
+      (resolve) => page.exposeFunction("readyToPrint", resolve),
+    ),
+    new Promise<void>(
+      (resolve) => setTimeout(
+        () => {
+          resolve();
+          console.warn(`Wait event for '${url}' is timed out`);
+        },
+        opts?.waitEventTimeout || DEFAULT_WAIT_EVENT_TIMEOUT,
+      ),
+    ),
+  ]);
 
   await page.goto(url, {waitUntil: "networkidle2"});
 
